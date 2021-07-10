ce109dbc7a3819a4091a506b3adb3eb543923f5b
Mary <bukva_zu@list.ru>
2021-07-10 16:53:46 +0300

    Task2 v1

--- src/test/java/guru/qa/FormTests.java
+++ src/test/java/guru/qa/FormTests.java
@@ -2,15 +2,16 @@ package guru.qa;
 
 
 
-import com.codeborne.selenide.Condition;
+//import com.codeborne.selenide.Condition;
+import static com.codeborne.selenide.Condition.text;
 import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.selector.ByText;
-import org.junit.jupiter.api.Assertions;
+
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 
-import static com.codeborne.selenide.Selectors.byCssSelector;
+import java.io.File;
+
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
@@ -24,7 +25,7 @@ public class FormTests {
     }
 
     @Test
-    void NameFillTest() {
+    void nameFillTest() {
         open("/automation-practice-form"); //перешли по ссылке
         $("#firstName").setValue("Mary"); //ввели имя
         $("#lastName").setValue("Esekhina"); //ввели фамилию
@@ -38,11 +39,10 @@ public class FormTests {
         $(".react-datepicker__month-select").selectOption("July");
         $(".react-datepicker__day--019").click();
         //выбираем предмет
-        $("#subjectsInput").setValue("English");
-        $("#subjectsInput").pressEnter();
+        $("#subjectsInput").setValue("English").pressEnter();
 
         $("#hobbiesWrapper").$(byText("Reading")).click(); //увлечения
-        $("#uploadPicture").setValue("K:\\wat.jpeg"); //фото
+        $("#uploadPicture").uploadFile(new File("src/test/java/../resources/wat.jpeg")); //фото
         $("#currentAddress").setValue("г. Рязань"); //адрес
         //выбираем штат
         $("#state").click();
@@ -52,10 +52,10 @@ public class FormTests {
         $("#submit").click();
 
         //проверка
-        $("#example-modal-sizes-title-lg").shouldHave(Condition.text("Thanks for submitting the form"));
-        $(".table-responsive").shouldHave(Condition.text("Mary Esekhina"), Condition.text("es@mail.ru"),
-                Condition.text("9101234567"), Condition.text("19 July,1990"),Condition.text("English"),
-                Condition.text("wat.jpeg"),Condition.text("г. Рязань"),Condition.text("Haryana Karnal"));
+        $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
+        $(".table-responsive").shouldHave(text("Mary Esekhina"), text("es@mail.ru"),
+                text("9101234567"), text("19 July,1990"), text("English"),
+                text("wat.jpeg"), text("г. Рязань"), text("Haryana Karnal"));
 
 
     }
